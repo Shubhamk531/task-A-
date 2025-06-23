9c8ad77c79c40f0507e0eecd000d046ece83a7ce
EC2 Default User <ec2-user@ip-172-31-12-51.ap-south-1.compute.internal>
2025-06-23 16:25:40 +0000

    Added content in branch

---
+++ dir1/file1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+read -p "Enter marks: " marks
+
+if [ "$marks" -gt 90 ]; then
+  echo "Distinction"
+elif [ "$marks" -ge 75 ]; then
+  echo "First class"
+elif [ "$marks" -ge 60 ]; then
+  echo "Second class"
+elif [ "$marks" -ge 35 ]; then
+  echo "Pass"
+else
+  echo "Fail"
+fi
+
